e3fef7d38a5abfb7d445955ee973ce787be69638
mfakhrim <mfakhrim643@gmail.com>
2024-06-09 22:00:05 +0700

    responsif pada detail panduan asuh

--- client/src/user/components/CardGizi.jsx
+++ client/src/user/components/CardGizi.jsx
@@ -1,9 +1,9 @@
 const CardGizi = ({ judul, desc }) => {
   return (
-    <div className="w-full max-w-sm md:px-4 bg-white border border-gray-200 rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700">
-      <div className="flex flex-col items-center justify-center py-5">
-        <h5 className="text-2xl mt-4  font-medium text-neutral-800 dark:text-white">{judul}</h5>
-        <div className="text-base flex mt-2 mx-10 font-normal text-center text-zinc-700">
+    <div className="w-full max-w-sm px-4 py-5 md:px-6 bg-white border border-gray-200 rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex flex-col items-center justify-center py-4">
+        <h5 className="text-2xl mt-4 font-medium text-neutral-800 dark:text-white ">{judul}</h5>
+        <div className="text-base flex mt-2 font-normal text-center text-zinc-700">
           <p>{desc}</p>
         </div>
       </div>
