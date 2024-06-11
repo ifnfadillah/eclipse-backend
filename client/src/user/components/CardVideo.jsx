ebe13b4bb7de39c9bacbfc412199b88b1cca0334
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2024-06-11 16:07:12 +0700

    add truncate title card vide belajar

--- client/src/user/components/CardVideo.jsx
+++ client/src/user/components/CardVideo.jsx
@@ -5,7 +5,16 @@ const CardVideo = ({ imageUrl, title, link }) => {
         <img className="w-full h-40 object-cover md:h-32 lg:h-48 rounded-t-lg" src={imageUrl} alt={title} />
         <div className="p-4 bg-sky-700 text-white text-left mx-auto hover:bg-sky-500 rounded-b-lg">
           <a href={link} target="_blank" rel="noopener noreferrer">
-            <h5 className="py-2 text-base md:text-xl font-semibold font-primary tracking-tight text-white">{title}</h5>
+            <h5 className="text-base md:text-xl font-semibold font-primary tracking-tight text-white"
+              style={{
+                display: "-webkit-box",
+                WebkitBoxOrient: "vertical",
+                WebkitLineClamp: 2,
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                lineHeight: "1.5em",
+                maxHeight: "3em",
+              }}>{title}</h5>
           </a>
         </div>
       </div>
